532c02f264918a21f1967cc28364d315cd2940c8
heqing111 <2637908704@qq.com>
2022-04-08 14:37:56 +0800

    三次提交

---
+++ src/main/java/com/nowcoder/community/util/CommunityConstant.java
@@ -0,0 +1,29 @@
+package com.nowcoder.community.util;
+
+/**
+ * @author coolsen
+ * @version 1.0.0
+ * @ClassName CommunityConstant.java
+ * @Description 常量
+ * @createTime 4/29/2020 3:33 PM
+ */
+public interface CommunityConstant {
+
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+    /**
+     * 重复激活
+     */
+    int ACTIVATION_REPEAT = 1;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
+
+
+
+}
